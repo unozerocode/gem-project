f874ca2414166821e7a05505cd9d2c3970cfd0c7
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-02-24 13:21:29 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,8 +53,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gem-project`,
+        short_name: `gem`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
