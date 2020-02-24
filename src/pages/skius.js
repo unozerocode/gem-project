6e751f65235930a735cf2525373678a693bf6f34
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-02-24 13:39:00 -0600

    Update skius.js

--- src/pages/skius.js
+++ src/pages/skius.js
@@ -1,69 +1,95 @@
 import React from 'react'
 import SkuCard from '../components/Products/SkuCard'
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Skeleton from '@material-ui/lab/Skeleton';
 const conatinerStyles = {
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
     padding: '1rem 0 1rem 0',
-  }
-  const styGrid = {
+}
+const styGrid = {
     flexWrap: 'wrap',
     padding: '1rem',
-  }
+}
 const stripe_url = 'https://api.stripe.com'
 
 const Skius = class extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            loading: true,
+            loading: false,
             error: false,
             fetchedData: [],
         }
-      }
-    
-    componentDidMount() {
+    }
 
+    componentDidMount() {
         this.getProducts();
     }
 
     getProducts() {
         fetch(stripe_url + "/v1/skus", {
             method: "GET",
-            headers: { 
-                'Host':'api.stripe.com',
-                'Accept':'*/*',
-                'Cache-Control':'no-cache',
+            headers: {
+                'Host': 'api.stripe.com',
+                'Accept': '*/*',
+                'Cache-Control': 'no-cache',
                 'Connection': 'keep-alive',
                 'Authorization': `Bearer ${process.env.GATSBY_STRIPE_S_KEY}`
             }
         }).then(response => {
             return response.json()
         }).then(json => {
-            console.log(json)
+
             this.setState({
-                fetchedData: json.data
+                fetchedData: json.data,
+
             })
+            if (this.state.fetchedData.length > 0) {
 
-        });
+                this.setState({ loading: true });
+            }
+            return json;
+        })
+
     }
+    createSkeleton = () => {
+        let table = []
 
+
+        for (let i = 0; i < 3; i++) {
+            table.push(<Grid item key={i} xs={6} sm={4} style={styGrid}>
+                <Box pt={0.5}>
+                    <Skeleton variant="text"  />
+                    <Skeleton variant="circle" width={40} height={40} />
+                    <Skeleton variant="rect" height={100} />
+                </Box>
+            </Grid>)
+        }
+        return table
+    }
     render() {
         const props = this.props;
-        const { fetchedData, loading } = this.state
-        return ( <div>
+        const { fetchedData } = this.state
+        return (<div>
             <Grid style={conatinerStyles}>
-             {fetchedData.map(sku => <Grid  key={sku.id}  item xs={6} sm={4} style={styGrid}>
-            <SkuCard {...props}sku={sku} />
-          </Grid>)}   
-            
-         
-          
-        </Grid>
-            
-            </div>
+                {this.state.loading ? (
+                    fetchedData.map(sku =>
+                        <Grid item key={sku.id} xs={6} sm={4} style={styGrid}>
+
+                            <SkuCard {...props} sku={sku} />
+
+                        </Grid>
+                    )
+
+                ) : (
+                        this.createSkeleton()
+                    )}
+            </Grid>
+        </div>
 
         )
     }
