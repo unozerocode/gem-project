ba74273350effa88431d2f8cbabcdfc42573449d
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-02-28 16:39:59 -0600

    update Cards

--- src/components/Products/SkuCard.js
+++ src/components/Products/SkuCard.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import ShoppingCart from '@material-ui/icons/ShoppingCart';
 
 
 const formatPrice = (amount, currency) => {
@@ -27,7 +33,7 @@ const SkuCard = class extends React.Component {
         this.setState({ disabled: false, buttonText: 'ADD ME BABY ONE MORE TIME!' })
     }
 
-    addToCart(event, skuId, quantity = 1) {
+    addToCart(event, skuId) {
         event.preventDefault()
         this.setState({ disabled: true, buttonText: 'ADDED...' })
         this.props.addToCart(skuId)
@@ -36,24 +42,42 @@ const SkuCard = class extends React.Component {
 
     render() {
         const sku = this.props.sku
+
         return (
 
-            <Card>
-            <CardContent >
 
-            < Typography variant = "h5"
-            component = "h2" > { sku.attributes.name } </Typography>
+            <Card style={{ maxWidth: `345` }} className="shadow rounded">
+                <CardHeader
+                    avatar={
+                        <Avatar aria-label="recipe" style={{ backgroundColor: red[500], }}>
+                            R
+          </Avatar>
+                    }
 
-            <Typography variant = "body2"
-            component = "p" >
-            Price: { formatPrice(sku.price, sku.currency) } </Typography> </CardContent>
-             <CardActions >
-            <Button variant = "contained"
-            color = "primary"
-            onClick = { event => this.addToCart(event, sku.id) }
-            disabled = { this.state.disabled }
-            size = "small" > { this.state.buttonText } < span className = "material-icons" > add_shopping_cart </span></Button >
-            </CardActions> { this.state.paymentMessage } </Card>
+                    title={sku.attributes.name}
+                    subheader={"Price: " + formatPrice(sku.price, sku.currency)}
+
+                />
+                <CardMedia
+                    style={{ height: `0`, paddingTop: `56.25%` }}
+                    image="gatsby-fut.jpg"
+                    title="Paella dish"
+                />
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        This impressive paella is a perfect party dish and a fun meal to cook together with your
+                        guests. Add 1 cup of frozen peas along with the mussels, if you like.
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+
+                    <IconButton aria-label="shopping cart">
+                        <ShoppingCart />
+                    </IconButton>
+
+                </CardActions>
+
+            </Card>
         )
     }
 }
