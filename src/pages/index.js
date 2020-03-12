0b522514e0f275abaf5a78f897fd2c7f2a711a03
Pineda A <32521420+GalPin-Ark@users.noreply.github.com>
2020-03-12 17:49:00 -0600

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,7 +66,7 @@ const IndexPage = () => {
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-lg-6 text-center">
-              hola
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </div>
 
 
